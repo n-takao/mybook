cac1f0ba95bdce04f12107d9222ab0d734c0e972
n-takao <ren615myu072@gmail.com>
2020-09-01 08:05:12 +0000

    mybook

---
+++ resources/views/admin/book/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>reader Book</title>
+    </head>
+    <body>
+        <h1>読本の紹介作成をする画面</h1>
+    </body>
+</html>
